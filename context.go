0393950c3acadf4ca2e3e14e7113edaafcb922cf
stella-go <stellago@yeah.net>
2023-12-11 11:19:47 +0800

    fix: use rewrite middleware

--- context.go
+++ context.go
@@ -177,7 +177,7 @@ func newEnvironmentContext(environment config.TypedConfig) *context {
 	ctx := newContext(environment, contextLogger, nil)
 	ctx.Register(&buildinRegister{ctx})
 	ctx.AutoFactory(&autoconfig.AutoMysql{}, &autoconfig.AutoRedis{}, &autoconfig.AutoZookeeper{})
-	ctx.Use(&middleware.MiddlewareAccess{}, &middleware.MiddlewareCROS{}, &middleware.MiddlewareErrorlog{}, &middleware.MiddlewareResource{}, &middleware.MiddlewareSession{})
+	ctx.Use(&middleware.MiddlewareRewrite{}, &middleware.MiddlewareAccess{}, &middleware.MiddlewareCROS{}, &middleware.MiddlewareErrorlog{}, &middleware.MiddlewareResource{}, &middleware.MiddlewareSession{})
 
 	return ctx
 }
